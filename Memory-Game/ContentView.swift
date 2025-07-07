4a7a9c9f29a3ec11264836ec8c02a2cef593e0dd
Guihenr1 <guilherme.pomp@gmail.com>
2025-07-07 07:01:44 +0100

    Initial Commit

---
+++ Memory-Game/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Memory-Game
+//
+//  Created by Guilherme Pompilio on 07/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("🤭").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memory-Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory-Game
+//
+//  Created by Guilherme Pompilio on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
