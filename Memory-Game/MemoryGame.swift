0ef772b5111c5e4acb2af7d287aa4d8b64f52ed7
Guihenr1 <guilherme.pomp@gmail.com>
2025-07-14 07:14:48 +0100

    Protocols, enum, Optional.

--- Memory-Game/MemoryGame.swift
+++ Memory-Game/MemoryGame.swift
@@ -7,29 +7,54 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: [Card]
     
     init(numberOfPairOfCards: Int, contentFactory: (Int) -> CardContent) {
         cards = []
         for pairIndex in 0..<max(2, numberOfPairOfCards) {
             let content = contentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: "\(pairIndex+1)a"))
+            cards.append(Card(content: content, id: "\(pairIndex+1)b"))
         }
     }
     
-    func choose (_ card: Card) {
-        
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { index in cards[index].isFaceUp }.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) } }
+    }
+    
+    mutating func choose (_ card: Card) {
+        if let chooseIndex = cards.firstIndex (where: { $0.id == card.id }) {
+            if !cards[chooseIndex].isFaceUp, !cards[chooseIndex].isMatched {
+                if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[chooseIndex].content == cards[potentialMatchIndex].content {
+                        cards[chooseIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chooseIndex
+                }
+                cards[chooseIndex].isFaceUp = true
+            }
+        }
     }
     
     mutating func shuffle() {
         cards.shuffle()
     }
     
-    struct Card {
-        var isFaceUp = true
+    struct Card: Equatable, Identifiable {
+        var isFaceUp = false
         var isMatched = false
         let content: CardContent
+        
+        var id: String
+    }
+}
+
+extension Array {
+    var only: Element? {
+        return count == 1 ? first : nil
     }
 }
