84b0e703f6f072b3332d46c31ee263f171dee354
Guihenr1 <guilherme.pomp@gmail.com>
2025-07-16 09:35:11 +0100

    Layout.

---
+++ Memory-Game/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memory-Game
+//
+//  Created by Guilherme Pompilio on 16/07/2025.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
